9547ebb6ce04ba135a903c9944935b01877f0758
psahir <psahir289@gmail.com>
2024-07-13 02:24:29 -0700

    db changes

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -9,26 +9,21 @@ class UserController {
         try {
             checkValidation(req)
             const data = req.body;
-            console.log("🚀 ~ UserController ~ createUser ~ data:", data)
 
             const isUserExist = await userRepo.getUserByUsernameRepo(data?.userName)
-            console.log("🚀 ~ UserController ~ createUser ~ isUserExist:", isUserExist)
 
             if (isUserExist){
                throw new CustomError(409,'User with this username already exist')
             }
 
             const hashedPassword =await hashPassword(req.body?.password)
-            console.log("🚀 ~ UserController ~ createUser ~ hashPassword:", hashPassword)
 
             const updatedData ={
                 ...data,
                 password:hashedPassword
             }
-            console.log("🚀 ~ UserController ~ createUser ~ data:", updatedData)
 
             const userRes = await userRepo.createUserRepo(data)
-            console.log("🚀 ~ UserController ~ createUser ~ data:", data)
             
             return DefaultResponse(
                 res,
@@ -37,7 +32,6 @@ class UserController {
                 userRes
             );
         } catch (error) {
-            console.log("🚀 ~ UserController ~ createUser ~ error:", error)
             next(error)
         }
     }
---
+++ prisma/migrations/20240713091933_acess_token/migration.sql
@@ -0,0 +1,57 @@
+/*
+  Warnings:
+
+  - The primary key for the `Merchant` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Permission` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `RolePermission` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "RolePermission" DROP CONSTRAINT "RolePermission_permissionId_fkey";
+
+-- AlterTable
+ALTER TABLE "Merchant" DROP CONSTRAINT "Merchant_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Merchant_id_seq";
+
+-- AlterTable
+ALTER TABLE "Permission" DROP CONSTRAINT "Permission_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Permission_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Permission_id_seq";
+
+-- AlterTable
+ALTER TABLE "RolePermission" DROP CONSTRAINT "RolePermission_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "permissionId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "RolePermission_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "RolePermission_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- CreateTable
+CREATE TABLE "AccessToken" (
+    "id" TEXT NOT NULL,
+    "accessToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AccessToken_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "RolePermission" ADD CONSTRAINT "RolePermission_permissionId_fkey" FOREIGN KEY ("permissionId") REFERENCES "Permission"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AccessToken" ADD CONSTRAINT "AccessToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
