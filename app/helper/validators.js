3e710ed162dba4806d660c9f2fb92485d67ebe77
psahir <psahir289@gmail.com>
2024-07-31 12:46:18 +0400

    changes done settlement

--- app/helper/validators.js
+++ app/helper/validators.js
@@ -1,6 +1,17 @@
 import { param } from "express-validator";
 import validator from 'express-validator'
-const { body } = validator
+const { body,query } = validator
+
+const methodEnums = ["BANK", "CASH", "CRYPTO", "AED"];
+const walletEnums = ["WALLET1", "WALLET2", "WALLET3"];
+const statusEnums = [
+  "INITIATED",
+  "ASSIGNED",
+  "SUCCESS",
+  "DROPPED",
+  "DUPLICATE",
+];
+
 
 export const permissionValidator = [
   body("name").notEmpty().withMessage("Name is required"),
@@ -230,3 +241,99 @@ export const validatePayInProcess = [
     .isFloat({ min: 0})
     .withMessage('amount must be a valid number between 0 and 100000')
 ];
+
+export const settlementCreateValidator = [
+  body("amount")
+    .trim()
+    .notEmpty()
+    .withMessage("Amount is required!")
+    .isNumeric()
+    .withMessage("Amount is invalid!"),
+  body("method")
+    .trim()
+    .notEmpty()
+    .withMessage("Method should be provided!")
+    .custom((method, meta) => {
+      if (!methodEnums.includes(method)) {
+        return Promise.reject(`Method is invalid! Should be one of these ${methodEnums}`)
+      }
+      const body = meta.req.body;
+      let validateFields = [];
+      switch (method) {
+        case "CRYPTO":
+          validateFields = ["wallet", "wallet_address"];
+          break;
+        case "BANK":
+          validateFields = ["acc_name", "acc_no", "ifsc"];
+          break;
+      }
+      for (const field of validateFields) {
+        const value = body[field];
+        if (!value || typeof value === "string" && !value.trim()) {
+          return Promise.reject(`${field} is required!`);
+        }
+        if (field == "ifsc" && !ifsc.validate(value)) {
+          return Promise.reject(`ifsc is invalid!`);
+        }
+        if (field == "wallet" && !walletEnums.includes(value)) {
+          return Promise.reject(`Wallet is invalid! Should be one of these ${walletEnums}`);
+        }
+      }
+      return Promise.resolve();
+    }),
+]
+
+export const settlementsGetValidator = [
+  query("id")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Id is required!"),
+  query("status")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("status is required!")
+    .isIn(statusEnums)
+    .withMessage(`Invalid status, Should be one of these ${statusEnums}`),
+  query("method")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("method is required!")
+    .isIn(methodEnums)
+    .withMessage(`Invalid method, Should be one of these ${methodEnums}`),
+  query("reference_id")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("reference_id is required!"),
+  query("acc_no")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("acc_no is required!"),
+  query("amount")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("amount is required!")
+    .isNumeric()
+    .withMessage("amount is invalid!"),
+  query("code")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("code is required!"),
+  query("page")
+    .optional()
+    .notEmpty()
+    .isNumeric()
+    .isInt({ min: 0 })
+    .withMessage("Invalid page!"),
+  query("take")
+    .optional()
+    .notEmpty()
+    .isNumeric()
+    .withMessage("Invalid take"),
+]
