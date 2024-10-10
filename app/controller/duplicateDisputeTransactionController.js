ce16c49c56cc9204a1e136af7dbf5478036cb2b4
Shadow <shadow77278837@gmail.com>
2024-10-10 09:42:37 +0400

    Bug resolved date filter and calculate commission

--- app/controller/duplicateDisputeTransactionController.js
+++ app/controller/duplicateDisputeTransactionController.js
@@ -3,24 +3,38 @@ import duplicateDisputeTransactionService from "../services/duplicateDisputeTran
 import { checkValidation } from "../helper/validationHelper.js";
 import payInServices from "../services/payInServices.js";
 import { sendTelegramDisputeMessage } from "../helper/sendTelegramMessages.js";
+import { calculateCommission } from "../helper/utils.js";
 import config from "../../config.js";
+import merchantRepo from "../repository/merchantRepo.js";
 
 class DuplicateDisputeTransactionController {
     async handleDuplicateDisputeTransaction(req, res, next) {
         try {
             checkValidation(req)
             const { payInId } = req.params;
-            const apiData = {
-                ...req.body,
-                status: "SUCCESS",
-            }
+            let apiData = {}
             const oldPayInData = await payInServices.getPayInDetails(payInId);
+            const merchantRes = await merchantRepo.getMerchantById(oldPayInData.merchant_id)
+            const payinCommission = await calculateCommission(oldPayInData?.amount, merchantRes?.payin_commission);
+            if (oldPayInData.status === "DUPLICATE") {
+                apiData = {
+                    ...req.body,
+                    status: "SUCCESS",
+                    payin_commission: payinCommission,
+                }
+            }
+            else {
+                apiData = {
+                    ...req.body,
+                    status: "SUCCESS",
+                }
+            }
             const duplicateDisputeTransactionRes = await duplicateDisputeTransactionService.handleDuplicateDisputeTransaction(payInId, apiData, oldPayInData.status);
             await sendTelegramDisputeMessage(
                 config?.telegramDuplicateDisputeChatId,
                 oldPayInData,
                 duplicateDisputeTransactionRes,
-                config?.telegramDuplicateDisputeChatId
+                config?.telegramBotToken
               );
             return DefaultResponse(res, 200, "Transaction updated successfully", duplicateDisputeTransactionRes);
         } catch (error) {
