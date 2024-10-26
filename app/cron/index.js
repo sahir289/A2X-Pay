9b4945eb44bc16285c7ccefdf93ed3eb89a8013c
psahir <psahir289@gmail.com>
2024-10-26 15:27:25 +0400

    bug: crons timezone bug fixed

--- app/cron/index.js
+++ app/cron/index.js
@@ -5,44 +5,47 @@ import config from "../../config.js";
 import moment from "moment-timezone";
 
 let canRunHourlyReports = false;
+
+// Schedule the daily task at 12:00 AM IST (midnight)
 cron.schedule("0 0 * * *", () => {
   gatherAllData("N", "Asia/Kolkata");
   canRunHourlyReports = true; 
+}, {
+  timezone: "Asia/Kolkata"
 });
+
+// Schedule the hourly task at every hour from 1:00 AM to 11:59 PM IST
 cron.schedule("0 1-23 * * *", () => {
   if (canRunHourlyReports) {
     gatherAllData("H", "Asia/Kolkata"); 
   }
+}, {
+  timezone: "Asia/Kolkata"
 });
-const gatherAllData = async (type = "N", timezone = "Asia/Kolkata")  => {
+
+const gatherAllData = async (type = "N", timezone = "Asia/Kolkata") => {
   try {
     const empty = "-- -- -- ";
     let startDate, endDate;
 
     const currentDate = moment().tz(timezone);
     if (type === "H") {
-            // Get the start and end times for the past hour in the specified timezone
+      // Get the start and end times for the past hour in the specified timezone
       startDate = currentDate.clone().subtract(1, "hour").toDate();
       endDate = currentDate.toDate();
     }
     if (type === "N") {
-            // Get the start and end times for the past 24 hours (or previous day)
+      // Get the start and end times for the past 24 hours (or previous day)
       endDate = currentDate.toDate();
       startDate = currentDate.clone().subtract(1, "days").startOf('day').toDate();
     }
-    //console.log(`Gathering data from ${startDate} to ${endDate} for type ${type} in ${timezone}`);
+
     const merchants = await prisma.merchant.findMany({
-      select: {
-        id: true,
-        code: true,
-      },
+      select: { id: true, code: true },
     });
 
     const banks = await prisma.bankAccount.findMany({
-      select: {
-        id: true,
-        ac_name: true,
-      },
+      select: { id: true, ac_name: true },
     });
 
     const merchantCodeMap = merchants.reduce((acc, merchant) => {
@@ -55,70 +58,44 @@ const gatherAllData = async (type = "N", timezone = "Asia/Kolkata")  => {
       return acc;
     }, {});
 
-    // Only fetch successful pay-ins
+    // Fetch successful pay-ins
     const payIns = await prisma.payin.groupBy({
       by: ["merchant_id"],
-      _sum: {
-        amount: true,
-      },
-      _count: {
-        id: true,
-      },
+      _sum: { amount: true },
+      _count: { id: true },
       where: {
         status: "SUCCESS", 
-        createdAt: {
-          gte: startDate,
-          lte: endDate,
-        },
+        createdAt: { gte: startDate, lte: endDate },
       },
     });
+
     const payOuts = await prisma.payout.groupBy({
       by: ["merchant_id"],
-      _sum: {
-        amount: true,
-      },
-      _count: {
-        id: true,
-      },
+      _sum: { amount: true },
+      _count: { id: true },
       where: {
         status: "SUCCESS", 
-        createdAt: {
-          gte: startDate,
-          lte: endDate,
-        },
+        createdAt: { gte: startDate, lte: endDate },
       },
     });
 
     const bankPayIns = await prisma.payin.groupBy({
       by: ["bank_acc_id"],
-      _sum: {
-        amount: true,
-      },
-      _count: {
-        id: true,
-      },
+      _sum: { amount: true },
+      _count: { id: true },
       where: {
         status: "SUCCESS", 
-        createdAt: {
-          gte: startDate,
-          lte: endDate,
-        },
+        createdAt: { gte: startDate, lte: endDate },
       },
     });
+
     const bankPayOuts = await prisma.payout.groupBy({
       by: ['from_bank'],
-      _sum: {
-        amount: true,
-      },
-      _count: {
-        id: true,
-      },
+      _sum: { amount: true },
+      _count: { id: true },
       where: {
         status: "SUCCESS",
-        createdAt: {
-          gte: startDate,
-          lte: endDate,
-        },
+        createdAt: { gte: startDate, lte: endDate },
       },
     });    
 
@@ -126,10 +103,7 @@ const gatherAllData = async (type = "N", timezone = "Asia/Kolkata")  => {
       .map((payIn) => {
         const { merchant_id, _sum, _count } = payIn;
         const merchantCode = merchantCodeMap[merchant_id];
-        if (merchantCode && _sum.amount > 0) {
-          return `${merchantCode}: ${formatePrice(_sum.amount)} (${_count.id})`;
-        }
-        return null;
+        return merchantCode && _sum.amount > 0 ? `${merchantCode}: ${formatePrice(_sum.amount)} (${_count.id})` : null;
       })
       .filter(Boolean);
 
@@ -137,10 +111,7 @@ const gatherAllData = async (type = "N", timezone = "Asia/Kolkata")  => {
       .map((payOut) => {
         const { merchant_id, _sum, _count } = payOut;
         const merchantCode = merchantCodeMap[merchant_id];
-        if (merchantCode) {
-          return `${merchantCode}: ${formatePrice(_sum.amount)} (${_count.id})`;
-        }
-        return null;
+        return merchantCode ? `${merchantCode}: ${formatePrice(_sum.amount)} (${_count.id})` : null;
       })
       .filter(Boolean);
     
@@ -148,35 +119,16 @@ const gatherAllData = async (type = "N", timezone = "Asia/Kolkata")  => {
       .map((payIn) => {
         const { bank_acc_id, _sum, _count } = payIn;
         const bankName = bankNamesMap[bank_acc_id];
-        if (bankName) {
-          return `${bankName}: ${formatePrice(_sum.amount)} (${_count.id})`;
-        }
-        return null;
+        return bankName ? `${bankName}: ${formatePrice(_sum.amount)} (${_count.id})` : null;
       })
       .filter(Boolean);
 
-      const formattedBankPayOuts = bankPayOuts
+    const formattedBankPayOuts = bankPayOuts
       .map((payOut) => {
         const { from_bank, _sum, _count } = payOut;
-        if (from_bank) {
-          return `${from_bank}: ${formatePrice(_sum.amount)} (${_count.id})`;
-        }
-        return null;
+        return from_bank ? `${from_bank}: ${formatePrice(_sum.amount)} (${_count.id})` : null;
       })
       .filter(Boolean);
-
-    // // Pay In
-    // console.log(`\nPayIns (${currentDate}) \n`);
-    // console.log(formattedPayIns.join("\n"));
-    // // Pay Out
-    // console.log(`\nPayOuts (${currentDate}) \n`);
-    // console.log(formattedPayOuts.join("\n"));
-    // // Bank Accounts PayIns
-    // console.log(`\nBank Accounts (${currentDate}) \n`);
-    // console.log(formattedBankPayIns.join("\n"));
-    // // Bank Accounts PayOuts
-    // console.log(`\nBank Accounts PayOut(${currentDate}) \n`);
-    // console.log(formattedBankPayOuts.join("\n"));
 
     await sendTelegramDashboardReportMessage(
       config?.telegramDashboardChatId,      
@@ -186,16 +138,12 @@ const gatherAllData = async (type = "N", timezone = "Asia/Kolkata")  => {
       formattedBankPayOuts,
       type === "H" ? "Hourly Report" : "Daily Report",
       config?.telegramBotToken,
-
     );
   } catch (err) {
-    console.log("========= CRON ERROR =========");
-    console.error(err);
-    console.log("==============================");
+    console.error("========= CRON ERROR =========", err);
   }
 };
-// gatherAllData("H");
-// gatherAllData("N");
+
 const formatePrice = (price) => {
   return Number(price).toLocaleString("en-US", {
     minimumFractionDigits: 2,
