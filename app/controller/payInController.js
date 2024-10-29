e422d53243305ce681c2ab95d3aff68a1d05733d
psahir <psahir289@gmail.com>
2024-10-29 11:45:43 +0400

    bug: resolved image upload issue

--- app/controller/payInController.js
+++ app/controller/payInController.js
@@ -947,7 +947,7 @@ class PayInController {
         image: base64Image,
       };
       const resFromOcrPy = await axios.post(
-        "http://34.196.43.192:8000/ocr",
+        "http://52.90.182.161:8000/ocr",
         imgData
       );
       // Merge the data from the API with the existing dataRes
@@ -1360,7 +1360,7 @@ class PayInController {
           image: base64Image,
         };
         const resFromOcrPy = await axios.post(
-          "http://34.196.43.192:8000/ocr",
+          "http://52.90.182.161:8000/ocr",
           imgData
         );
 
