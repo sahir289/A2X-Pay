8bfe5a90e9f3059af2dd11e6e70acb598cdff4b8
psahir <psahir289@gmail.com>
2024-11-12 16:39:53 +0400

    bug: resolved reset button functionality

--- app/controller/duplicateDisputeTransactionController.js
+++ app/controller/duplicateDisputeTransactionController.js
@@ -6,6 +6,7 @@ import { sendTelegramDisputeMessage } from "../helper/sendTelegramMessages.js";
 import { calculateCommission } from "../helper/utils.js";
 import config from "../../config.js";
 import merchantRepo from "../repository/merchantRepo.js";
+import payInRepo from "../repository/payInRepo.js";
 
 class DuplicateDisputeTransactionController {
     async handleDuplicateDisputeTransaction(req, res, next) {
@@ -14,19 +15,47 @@ class DuplicateDisputeTransactionController {
             const { payInId } = req.params;
             let apiData = {}
             const oldPayInData = await payInServices.getPayInDetails(payInId);
+            const oldPayInUtrData = oldPayInData?.user_submitted_utr;
             const merchantRes = await merchantRepo.getMerchantById(oldPayInData.merchant_id)
             const payinCommission = calculateCommission(oldPayInData?.confirmed, merchantRes?.payin_commission);
+
             const durMs = new Date() - oldPayInData?.createdAt;
             const durSeconds = Math.floor((durMs / 1000) % 60).toString().padStart(2, '0');
             const durMinutes = Math.floor((durSeconds / 60) % 60).toString().padStart(2, '0');
             const durHours = Math.floor((durMinutes / 60) % 24).toString().padStart(2, '0');
             const duration = `${durHours}:${durMinutes}:${durSeconds}`;
-            if (oldPayInData.status === "DUPLICATE") {
-                apiData = {
-                    ...req.body,
-                    status: "SUCCESS",
-                    payin_commission: payinCommission,
-                    duration: duration,
+
+            if (oldPayInData?.status === "DUPLICATE") {
+                let duplicateSuccess = false;
+                const allDeposits = await payInRepo.getPayinDataByUsrSubmittedUtr(oldPayInData?.user_submitted_utr);
+                    allDeposits?.map( async (value, index) => {
+                        if(value?.status !== 'SUCCESS' && value?.status !== 'DUPLICATE'){
+                            duplicateSuccess = true;
+
+                            apiData = {
+                                utr: req?.body?.utr,
+                                user_submitted_utr: req?.body?.utr,
+                                status: "DUPLICATE",
+                            }
+                            await payInRepo.updatePayInData(value?.id, apiData)
+                        }
+                    })
+                if(duplicateSuccess === true){
+                    apiData = {
+                        utr: oldPayInUtrData,
+                        user_submitted_utr: oldPayInUtrData,
+                        status: "SUCCESS",
+                        payin_commission: payinCommission,
+                        duration: duration,
+                    }
+                    duplicateSuccess = false;
+                } else {
+                    apiData = {
+                        ...req.body,
+                        status: "SUCCESS",
+                        payin_commission: payinCommission,
+                        duration: duration,
+                    }
                 }
             }
             else {
