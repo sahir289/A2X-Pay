05cfda9b2b4cf7b3f2d9d145b31f62ed3c854722
Shadow <shadow77278837@gmail.com>
2024-11-20 00:05:45 +0400

    Bank Miss Match Bug Resolved

--- app/controller/payInController.js
+++ app/controller/payInController.js
@@ -2762,10 +2762,12 @@ class PayInController {
       const durMinutes = Math.floor((durSeconds / 60) % 60).toString().padStart(2, '0');
       const durHours = Math.floor((durMinutes / 60) % 24).toString().padStart(2, '0');
       const duration = `${durHours}:${durMinutes}:${durSeconds}`;
+      const getBank = await bankAccountRepo.getBankNickName(bank_name);
   
       const updatePayInData = {
         status: "SUCCESS",
         bank_name: bank_name,
+        bank_acc_id: getBank.id,
         amount: payInData.confirmed,
         payin_commission: payinCommission,
         duration: duration,
@@ -2776,6 +2778,11 @@ class PayInController {
       await botResponseRepo.updateBotResponseByUtr(
         getBankResponseByUtr?.id,
         getBankResponseByUtr?.utr
+      );
+
+      await bankAccountRepo.updateBankAccountBalance(
+        getBank?.id,
+        parseFloat(payInData.confirmed)
       );
 
       const notifyData = {
