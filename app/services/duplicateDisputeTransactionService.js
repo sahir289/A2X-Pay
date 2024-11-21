4f500f76ccc059d7fcb5dc82f15ef5a0396e6715
Shadow <shadow77278837@gmail.com>
2024-11-21 18:16:50 +0400

    Bug fixed
    1. public_api_key in merchant module
    2. add callback for dispute/duplicate reset

--- app/services/duplicateDisputeTransactionService.js
+++ app/services/duplicateDisputeTransactionService.js
@@ -12,6 +12,28 @@ class DuplicateDisputeTransactionService {
         else if (prevStatus === "DISPUTE") {
              const merchantRes = await merchantRepo.updateMerchant(duplicateDisputeTransactionData.merchant_id, duplicateDisputeTransactionData.amount)
         }
+        
+        const notifyData = {
+            status: duplicateDisputeTransactionData?.status,
+            merchantOrderId: duplicateDisputeTransactionData?.merchant_order_id,
+            payinId: duplicateDisputeTransactionData?.id,
+            amount: duplicateDisputeTransactionData?.confirmed,
+            req_amount: duplicateDisputeTransactionData?.amount,
+            utr_id: duplicateDisputeTransactionData?.utr
+        };
+        
+        try {
+            logger.info('Sending notification to merchant', { notify_url: updatedPayInData?.notify_url, notify_data: notifyData });
+            //When we get the notify url we will add it.
+            const notifyMerchant = await axios.post(updatedPayInData?.notify_url, notifyData);
+            logger.info('Sending notification to merchant', {
+                status: notifyMerchant.status,
+                data: notifyMerchant.data,
+            })
+
+        } catch (error) {
+            logger.error("Error sending notification:", error);
+        }
         return duplicateDisputeTransactionData;
     }
 }
