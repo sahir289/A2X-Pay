90bebe5047b6e9eb01ba9c7e1b8468529d214fc2
Shadow <shadow77278837@gmail.com>
2024-12-24 14:20:13 +0400

    Error Handling in User Module

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,13 +1,15 @@
 import userRepo from "../repository/userRepo.js";
+import { logger } from "../utils/logger.js";
 
 class UserService {
 
-  async getAllUsers(skip,take,fullName,userName,role, createdBy, loggedInUserRole) {
-
-    const users = await userRepo.getAllUsers(skip, take, fullName, userName, role, createdBy, loggedInUserRole)
-
-    return users
-
+  async getAllUsers(skip, take, fullName, userName, role, createdBy, loggedInUserRole) {
+    try {
+      const users = await userRepo.getAllUsers(skip, take, fullName, userName, role, createdBy, loggedInUserRole);
+      return users;
+    } catch (error) {
+      logger.info('Error fetching users:', error.message);
+    }
   }
 }
 
