1b7cd7727397664251f7b1cba20cbe3903333f81
Shadow <shadow77278837@gmail.com>
2024-12-30 16:23:09 +0400

    Vendor Account Report Added

--- app/routes/weeklyReportRouter.js
+++ app/routes/weeklyReportRouter.js
@@ -4,5 +4,6 @@ import weeklyReportController from '../controller/weeklyReportController.js';
 const weeklyReportRouter = express()
 
 weeklyReportRouter.get('/weekly-report', weeklyReportController.getWeeklyReport )
+weeklyReportRouter.get('/weekly-vendor-report', weeklyReportController.getWeeklyVendorReport )
 
 export default weeklyReportRouter;
