90f1c77066db16457a93c850e08f9c6eb63c585a
psahir <psahir289@gmail.com>
2025-01-16 16:43:57 +0400

    added consoles

--- app/webhooks/razorpay.js
+++ app/webhooks/razorpay.js
@@ -15,6 +15,7 @@ const razorpay = new Razorpay({
 const RazorHook = async (req, res) => {
     try {
         const webhookSecret = 'trust-pay-stg#001188';
+        console.log(req.headers, "req.headers")
         const receivedSignature = req.headers['X-Razorpay-Signature'];
         const data = req.body?.payload?.payment?.entity || {};
         validateWebhookSignature(JSON.stringify(req.body), receivedSignature, webhookSecret);
